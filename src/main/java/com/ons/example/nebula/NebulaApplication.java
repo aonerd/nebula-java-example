e7364ac3d280f5d23443099e4e85d90a632229e4
Aiden O'Neill <aiden.oneill@synchronoss.com>
2018-07-26 17:14:53 +0100

    first commit

---
+++ src/main/java/com/ons/example/nebula/NebulaApplication.java
@@ -0,0 +1,12 @@
+package com.ons.example.nebula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NebulaApplication.class, args);
+	}
+}
